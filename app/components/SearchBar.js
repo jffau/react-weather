97cff82e057bdbe912b9b3ff7e8da0433ca35199
Jeff Au <jeffau13@gmail.com>
2019-12-07 22:19:49 -0500

    google maps autocomplete input

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,16 +1,53 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
+import Script from "react-load-script";
+
 import { LocationContext } from "../contexts/locations";
+
 const SearchBar = () => {
   const [input, setInput] = useState("");
+
   const { addLocation } = useContext(LocationContext);
+  const autocomplete = useRef();
+
   const handleSubmit = () => {
     addLocation(input);
     setInput("");
   };
+
+  const handleScript = () => {
+    let options = {
+      types: ["(regions)"]
+    };
+    // to disable any eslint 'google not defined' errors
+    // init google autocomplete
+
+    autocomplete.current = new google.maps.places.Autocomplete(
+      document.getElementById("autocomplete"),
+      options
+    );
+    // enableEnterKey(field);
+
+    // Fire Event when a suggested name is selected
+    autocomplete.current.addListener("place_changed", handlePlaceSelect);
+  };
+
+  const handlePlaceSelect = async () => {
+    let addressObject = await autocomplete.current.getPlace();
+    console.log(addressObject.formatted_address);
+    console.log(addressObject.geometry.location.lat());
+    console.log(addressObject.geometry.location.lng());
+  };
+
   return (
     <div>
+      <Script
+        url={`https://maps.googleapis.com/maps/api/js?key=${"AIzaSyDIiMlJk5jdhP-kOEDJcNEDeMoyGBszRWA"}&libraries=places`}
+        onLoad={handleScript}
+      />
+
       <input
         type="text"
+        id="autocomplete"
         placeholder="input location"
         value={input}
         onChange={e => setInput(e.target.value)}
